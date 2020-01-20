cbd315e35cf570d9cdb9c3c7726e81f5a336f879
Patrik <putte.karlsson@gmail.com>
2020-01-20 13:56:53 +0100

    Edited file: classes.py
    created class Deck

--- kmom03/war/classes.py
+++ kmom03/war/classes.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
     names = [None, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack",
     "Queen", "King"]
@@ -10,6 +12,30 @@ class Card:
     def __str__(self):
         return "{} {}".format(self.names[self.value], self.suits[self.suit])
 
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.reset_cards()
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def take_card(self):
+        return self.cards.pop()
+
+    def add_card(self, card):
+        self.cards.insert(0, card)
+
+    def reset_cards(self):
+        self.cards = []
+        for suit in range(4):
+            for value in range(1, 14):
+                self.cards.append(Card(value, suit))
+
 if __name__ == "__main__":
     c = Card(1,1)
-    print(c)
+    d = Deck()
+    # d.shuffle()
+    c = d.take_card()
+    d.add_card(c)
+    print(d.cards[0])
