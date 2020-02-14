7f7dd8812f5cab550dd146b715387096667af8e1
Patrik <putte.karlsson@gmail.com>
2020-02-14 20:19:47 +0100

    Kmom06 get method done

--- kmom06/tree/bst.py
+++ kmom06/tree/bst.py
@@ -2,22 +2,18 @@ from node import Node
 
 
 class BinarySearchTree():
-    """ BinarySearchTree class """
-
-
+    """ .sd. """
 
     def __init__(self):
-        """ Initialize BinarySearchTree """
+        """ ### """
 
         self.root = None
         self.size = 0
 
 
 
-    def insert(self, key, value, node):
-        """  
-        Skapar en ny nod med key och value,. ägger till en noden på rätt plats i trädet, baserat på key.
-        """ 
+    def insert(self, key, value):
+        """ ### """
 
         if self.root is None:
             self.root = Node(key, value)
@@ -28,6 +24,8 @@ class BinarySearchTree():
 
     @staticmethod
     def _insert(key, value, node):
+        """ ### """
+
         if key < node:
             if node.has_left_child():
                 BinarySearchTree._insert(key, value, node.left)
@@ -38,29 +36,70 @@ class BinarySearchTree():
                 BinarySearchTree._insert(key, value, node.right)
             else:
                 node.right = Node(key, value, node)
-        elif key == node:
-            node.value == value
+        else:
+            node.value = value
 
 
 
     def inorder_traversal_print(self):
-        """ 
-        Skriver ut en noderna i trädet i rätt ordning, lågt till högt.
+        """ ### """
+
+        self._print_nodes(self.root)
+
+
+
+    @staticmethod
+    def _print_nodes(node):
+        """ ### """
+
+        if node.has_left_child():
+            BinarySearchTree._print_nodes(node.left)
+        
+        print(node.value)
+        
+        if node.has_right_child():
+            BinarySearchTree._print_nodes(node.right)
+
+
+
+    def get(self, key):
         """
-        pass 
-
-
-
-    def get(self, key): # Node.value
+        Returnera value från noden med nyckeln key. Om key inte finns i 
+        trädet lyft ett KeyError exception (det inbyggda).
         """
-        Returnera value från noden med nyckeln key. Om key inte finns i trädet lyft ett KeyError exception (det inbyggda).
+
+        return self._get(self.root, key).value
+
+
+
+    @staticmethod
+    def _get(node, key):
+        """ asdasd """
+
+        if node.key == key:
+            return node
+        else:
+            raise KeyError("No matching key found!")
+
+        if node.has_left_child():
+            BinarySearchTree._get(node.left, key)
+
+        if node.has_right_child():
+            BinarySearchTree._get(node.right, key)
+
+
+
+    def remove(self):
         """ 
+        Ta bort nod med samma key, returnera värdet från noden. 
+        Om nod med key inte finns lyft KeyError exception (det inbyggda). 
+        """
+
         pass
 
-
-
-    def remove(self, key): # Node.value
-        """
-        Ta bort nod med samma key, returnera värdet från noden. Om nod med key inte finns lyft KeyError exception (det inbyggda).
-        """
-        pass
+bst = BinarySearchTree()
+bst.insert(8, "first")
+bst.insert(4, "second")
+bst.insert(11, "third")
+#bst.inorder_traversal_print()
+print(bst.get(9))
--- kmom06/tree/node.py
+++ kmom06/tree/node.py
@@ -7,24 +7,68 @@ class Node():
     def __init__(self, key, value, parent=None):
         """ init """
 
-        self.key
-        self.value
+        self.key = key
+        self.value = value
         self.left = None
         self.right = None
         self.parent = parent
 
+
+
     def has_left_child(self):
+        """ returnera True om noden har en left child nod, annars False. """
+
         return self.left is not None
 
 
 
     def has_right_child(self):
+        """ returnera True om noden har en right child nod, annars False. """
+
         return self.right is not None
 
 
 
+    def has_both_children(self):
+        """
+        returnera True om noden har en left och en right child nod, annars False.
+        """
+
+        return self.right and self.left is not None
+
+
+
+    def has_parent(self):
+        """
+        returnera True om noden har en parent nod, annars False.
+        """
+
+        pass
+
+
+
+    def is_left_child(self):
+        """
+        returnera True om noden är left child till sin parent nod, annars False.
+        """
+
+        pass
+
+
+
+    def is_right_child(self):
+        """
+        returnera True om noden är right child tills sin parent nod, annars False.
+        """
+
+        pass
+
+
+
     def __lt__(self, other):
-        if isinstance(other, node):
+        """ returnera True om nodens key är mindre än other, annars False. """
+
+        if isinstance(other, Node):
             return self.key < other.key
         else:
             return self.key < other
@@ -32,7 +76,9 @@ class Node():
 
 
     def __gt__(self, other):
-        if isinstance(other, node):
+        """ returnera True om nodens key är större än other, annars False. """
+
+        if isinstance(other, Node):
             return self.key > other.key
         else:
             return self.key > other
@@ -40,7 +86,9 @@ class Node():
 
 
     def __eq__(self, other):
-        if isinstance(other, node):
+        """ returnera True om nodens key är lika med other, annars False. """
+
+        if isinstance(other, Node):
             return self.key == other.key
         else:
             return self.key == other
